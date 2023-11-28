542b04dfcada893f2905eb13799ada444534c8b1
Alven <ooonush@gmail.com>
2023-11-28 18:59:25 +0500

    Added custom serializers for numbers

---
+++ Assets/Plugins/Stats/Addons/FishyStats/Runtime/Serializers/StatNumberSerializer.cs
@@ -0,0 +1,16 @@
+﻿using FishNet.Serializing;
+
+namespace Stats.FishNet
+{
+    public static class StatNumberSerializer
+    {
+        public static void WriteTInt(this Writer writer, TInt value) => writer.WriteInt32(value);
+        public static TInt ReadTInt(this Reader reader) => reader.ReadInt32();
+
+        public static void WriteTFloat(this Writer writer, TFloat value) => writer.WriteSingle(value);
+        public static TFloat ReadTFloat(this Reader reader) => reader.ReadSingle();
+
+        public static void WriteTDouble(this Writer writer, TDouble value) => writer.WriteDouble(value);
+        public static TDouble ReadTDouble(this Reader reader) => reader.ReadDouble();
+    }
+}
